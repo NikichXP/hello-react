b0f7bba97f32369c5880d6886246f48a629c1fda
Nikita <nikichx2@gmail.com>
2023-05-09 23:34:48 +0200

    rendering charts

--- src/components/ChartsPage.js
+++ src/components/ChartsPage.js
@@ -1,14 +1,42 @@
 import {LineChart, Line, ResponsiveContainer, CartesianGrid, XAxis, YAxis, Tooltip, Legend} from 'recharts'
+import {useEffect, useState} from "react";
 
-const src = [1, 2, 3, 4, 5, 6, 4, 3, 2, 6, 1, 7, 1, 4, 2, 1]
+async function getChartIds() {
+    return fetch('https://route.nikichxp.xyz/charts/list')
+        .then(response => response.json())
+        .then(arr => arr.map(el => el.id))
+}
 
 export function ChartsPage() {
 
+    const [charts, setCharts] = useState(null)
+    const [src, setSrc] = useState([1])
+
+    const fetchChartData = () => {
+        fetch('https://route.nikichxp.xyz/charts/ROUTE_OK?grouping=10')
+            .then(response => response.json())
+            .then(json => setSrc(json.chartData))
+    }
+
+    if (charts == null) {
+        getChartIds()
+            .then(setCharts)
+            .then(fetchChartData)
+    }
+
+    useEffect(() => {
+        const interval = setInterval(fetchChartData, 10_000)
+        return () => clearInterval(interval)
+    }, [])
+
     let data = src.map((num, i) => {
-        return {name: i, data: num, mod: 1 + (num % 5)}
+        return {name: i, data: num}
     })
 
-    return <div style={ {width:"50%", height:"50%" }}>
+    return <div>
+        <button onClick={() => getChartIds().then(setCharts)}>Update charts list</button><br/>
+        <span>{JSON.stringify(charts)}</span><br/>
+        <span>{JSON.stringify(src)}</span><br/>
         <LineChart
             width={1200}
             height={700}
@@ -20,7 +48,7 @@ export function ChartsPage() {
             <Tooltip/>
             <Legend/>
             <Line type="monotone" dataKey="data" stroke="#8884d8"/>
-            <Line type="monotone" dataKey="mod" stroke="#82ca9d"/>
+            {/*<Line type="monotone" dataKey="mod" stroke="#82ca9d"/>*/}
         </LineChart>
     </div>
 }
